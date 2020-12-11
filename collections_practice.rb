a61241b640fb771e75bb54d5e0985acfd0532569
Roy Mosby <ego.madking@gmail.com>
2020-12-11 09:34:44 -0500

    commit tree 5112bda3ed1a2041212ae65b18d245a482c6f310

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort { |a, b| b <=> a }
+end
+
+def sort_array_char_count(array)
+  array.sort { |a, b| a.length <=> b.length }
+end
+
+def swap_elements(array)
+  array << array.slice!(-2)
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.map do |element|
+    element.gsub(element[2], "$")
+  end
+end
+
+def find_a(array)
+  array.filter do |element|
+    element.start_with?('a')
+  end
+end
+
+def sum_array(array)
+  numbers = array.filter do |element|
+    element.integer?
+  end
+  numbers.inject { |sum, num| sum + num }
+end
+
+def add_s(array)
+  array.map.with_index do |item, i|
+    if i != 1
+      item << "s"
+    else
+      item
+    end
+  end
+end
